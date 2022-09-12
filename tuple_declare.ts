e3cac54bf749f8b63c7dec20638da34042db2d76
Your Name <deva162501@gmail.com>
2022-09-12 16:44:13 +0530

    tuple declare & init

---
+++ tuple_declare.ts
@@ -0,0 +1,21 @@
+
+let employee:[number,string] = [1,"steve"];
+console.log(employee);
+
+let person:[number,string,boolean] = [1,"steve",true];
+console.log(person);
+
+let user:[number,string,boolean,number,string];
+user = [1,"steve",true,20,"admin"];
+console.log(user);
+
+let emp:[number,string][];
+emp = [[1,"Steve"],[2,"Bell"],[3,"Jeff"]];
+console.log(emp); 
+
+employee.push(2,"Bill");
+console.log(employee);
+
+
+
+
