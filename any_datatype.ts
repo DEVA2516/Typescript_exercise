7edae7aeb89ea9e6e83090a0a051af67205af84c
Your Name <deva162501@gmail.com>
2022-09-13 11:03:48 +0530

    void function

---
+++ any_datatype.ts
@@ -0,0 +1,20 @@
+let variable : any = "hello wrold";
+console.log(variable);
+
+variable = 23;
+console.log(variable);
+
+variable = true;
+console.log(variable);
+
+variable =[1,23,3];
+console.log(variable);
+
+variable = [1,"s",2,"2",true];
+console.log(variable);
+
+let arr : any[] = [1,23,4,1]
+console.log(arr);
+
+arr = [1,"123","asaa",1,false,[1,2]];
+console.log(arr);
