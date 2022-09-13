6acf45570cac4988da92dd1a11d566b32e3c8b40
Your Name <deva162501@gmail.com>
2022-09-13 14:20:58 +0530

    sum function

--- sum_func.ts
+++ sum_func.ts
@@ -3,3 +3,17 @@ let sum = function (x:number, y:number): number {
 }
 
 console.log(sum(2,3));
+
+function Greet(greeting : string, name:string) : string {
+    return greeting + " "+ name;
+}
+
+console.log(Greet('Hello','steve'))
+
+// // console.log(Greet('hello'))
+
+let sumArrow = ((a:number,b:number) => {
+    return a + b;
+   })
+   
+   console.log(sumArrow(2,3))
