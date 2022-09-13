f1fc1695604d40590cc45145b901f075209bba76
Your Name <deva162501@gmail.com>
2022-09-13 10:00:31 +0530

    string enum

---
+++ string_enum.ts
@@ -0,0 +1,11 @@
+enum PrintMedia {
+    Newspaper = "NEWSPAPER",
+    Newsletter = "NEWSLETTER",
+    Magazine = "MAGAZINE",
+    Book = "BOOK"
+}
+
+console.log(PrintMedia.Newspaper)
+console.log(PrintMedia['Magazine']);
+
+
