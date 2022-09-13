1c055303fef4eb2ea97ed965c4ff12f489e78447
Your Name <deva162501@gmail.com>
2022-09-13 17:09:49 +0530

    interface read only property

---
+++ read_only_prob.ts
@@ -0,0 +1,12 @@
+interface Citizen {
+    name:string;
+    readonly SSN:number;
+}
+
+let personObj: Citizen = { SSN:110544,name:'James Bond'}
+console.log(personObj);
+
+personObj.name = "Bond";
+//personObj.SSN = 12345678;
+
+console.log(personObj);
