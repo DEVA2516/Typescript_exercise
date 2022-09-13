ac7377255310dbc29e2f59d0eb7448144866349c
Your Name <deva162501@gmail.com>
2022-09-13 15:49:13 +0530

    interface function

---
+++ keyvalueprocessor.ts
@@ -0,0 +1,16 @@
+interface KeyValueProcesser {
+    (key:number, value:string):void;
+};
+
+function addKeyValue(key:number,value:string):void {
+    console.log("addKeyValue: key = "+ key +" value = "+ value);
+} 
+
+function updateKeyValue(key:number,value:string):void {
+    console.log("updateKeyValue: key ="+ key +" value = "+ value);
+}
+
+let kvp:KeyValueProcesser = addKeyValue;
+kvp(1,"steve");
+kvp = updateKeyValue;
+kvp(2,"bill")
